52fb29527519b15062c2ce3a50a66f8c1d0e48bf
Jakub Ledwoń <ledwojakub@gmail.com>
2022-04-04 22:12:12 +0200

    Add basic admin module

--- apps/characters/admin.py
+++ apps/characters/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from characters.models import CharactersCsvFile
+
+@admin.decorators.register(CharactersCsvFile)
+class CharactersCsvFileAdmin(admin.ModelAdmin):
+    list_display = ("id", "ctime", "mtime")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
